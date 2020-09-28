c4d6542ff384d914315714873ff4d7c000140485
BSoldi <briansoldani@gmail.com>
2020-09-28 00:35:28 -0700

    make better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Serve up static assets (usually on heroku)
+// Serves the static assets => heroku
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, function() {
